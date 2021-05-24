0a0d8cd483b81c35bbeec828f6d38173bf620f48
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-24 16:45:52 +0930

    feat: add cookie session to server

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,3 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
 
 export const app = express();
+
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(cookieSession({
+  signed: false,
+  secure: true,
+}));
