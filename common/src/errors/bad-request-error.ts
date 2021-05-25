0a3796e4bd997700c12555ea66c2b469a9d1307e
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-25 14:55:23 +0930

    feat: add badRequestError

---
+++ common/src/errors/bad-request-error.ts
@@ -0,0 +1,15 @@
+import { CustomError } from "./interface/custom-error";
+
+export class BadRequesterror extends CustomError {
+  statusCode = 400
+
+  constructor(public message: string) {
+    super(message);
+    Object.setPrototypeOf(this, BadRequesterror.prototype)
+  }
+
+  serializeErrors() {
+    return [{message: this.message}]
+  }
+  
+}
