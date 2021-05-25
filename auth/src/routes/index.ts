c60259b8e2b36ad2122d3687bab9a04b574e21a5
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-25 16:16:03 +0930

    feat: return 400 if password is empty

--- auth/src/routes/index.ts
+++ auth/src/routes/index.ts
@@ -7,4 +7,9 @@ export const authRouter = express.Router();
 
 authRouter.post('/api/users/signup', [
   body('email').isEmail().withMessage('Email must be valid'),
+  body('password').trim().notEmpty().isLength({
+    min: 6,
+    max: 20,
+  })
+    .withMessage('Password is required and must have at least 6 characters'),
 ], validateRequest, signUp);
