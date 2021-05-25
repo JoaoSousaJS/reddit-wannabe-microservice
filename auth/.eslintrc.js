2533b20595ea79e93dd9e893f0b56ccafebb5fc0
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-25 11:42:34 +0930

    fix: fix eslint

--- auth/.eslintrc.js
+++ auth/.eslintrc.js
@@ -1,36 +1,41 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base'],
+  parser: '@typescript-eslint/parser',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
     },
-    "extends": "airbnb-base",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "settings": {
-      "import/resolver": {
-        "node": {
-          "extensions": [".js", ".jsx", ".ts", ".tsx"]
-        }
-      }
-    },
-    "rules": {
-      "import/prefer-default-export": "off",
-      "no-console": "off",
-      "import/extensions": [
-        "error",
-        "ignorePackages",
-        {
-          "js": "never",
-          "jsx": "never",
-          "ts": "never",
-          "tsx": "never"
-        }
-     ]
-    }
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'no-console': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
 };
