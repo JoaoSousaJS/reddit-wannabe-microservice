4518fee83f5e86ff001175a8caab39b86e0d4e0e
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-25 15:47:02 +0930

    test: return 400 if email is invalid or blank

--- auth/src/controllers/users/__test__/new-uset.spec.ts
+++ auth/src/controllers/users/__test__/new-uset.spec.ts
@@ -2,3 +2,12 @@ import request from 'supertest';
 import { app } from '../../../app';
 
 const agent = request.agent(app);
+
+describe('New User Controller', () => {
+  it('should return 400 if email is invalid or blank', async () => {
+    await agent.post('/api/users/signup').send({
+      email: '',
+      password: 'asdfasdfasd',
+    }).expect(400);
+  });
+});
