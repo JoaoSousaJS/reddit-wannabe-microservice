41c2b144b3278e83444912e22af24df772725527
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-25 16:36:06 +0930

    feat: return 400 if password and password confirmation are not equal

--- auth/src/controllers/users/new-user.ts
+++ auth/src/controllers/users/new-user.ts
@@ -1,5 +1,11 @@
+import { BadRequesterror } from '@reddit-wannabe/common';
 import { Request, Response } from 'express';
 
 export const signUp = async (req: Request, res: Response) => {
+  const { password, confirmPassword } = req.body;
+
+  if (password !== confirmPassword) {
+    throw new BadRequesterror('Password and Password confirmation must be equal');
+  }
   res.status(201).send({ status: true });
 };
