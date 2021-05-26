5a8df7bbcad4008c319914ff51e5a5d41c19ab55
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-26 10:23:34 +0930

    feat: add exporter

--- common/src/index.ts
+++ common/src/index.ts
@@ -3,3 +3,4 @@ export * from './errors/interface/custom-error'
 export * from './errors/request-validation-error'
 
 export * from './middlewares/validate-request'
+export * from './middlewares/error-handler'
