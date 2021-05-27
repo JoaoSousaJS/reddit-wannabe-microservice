40487d3906d974380a90f8ab9efe9480999e8eca
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-27 15:22:14 +0930

    test: return a cookie if credentials are valid

--- auth/src/controllers/users/__test__/sign-in.spec.ts
+++ auth/src/controllers/users/__test__/sign-in.spec.ts
@@ -1,6 +1,5 @@
 import request from 'supertest';
 import { app } from '../../../app';
-import { User } from '../../../database/models/user';
 import { clear, close, connect } from '../../../test/setup';
 
 const agent = request.agent(app);
@@ -51,5 +50,22 @@ describe('Sign in Controller', () => {
       email: 'joao@asdas.com',
       password: 'asdfg123456',
     }).expect(400);
+  });
+
+  it('should return a cookie when credentials are valid', async () => {
+    await agent.post('/api/users/signup').send({
+      firstName: 'joao',
+      lastName: 'sousa',
+      email: 'joao@asdas.com',
+      password: 'asdfg1234',
+      confirmPassword: 'asdfg1234',
+    }).expect(201);
+
+    const response = await agent.post('/api/users/signin').send({
+      email: 'joao@asdas.com',
+      password: 'asdfg1234',
+    }).expect(200);
+
+    expect(response.get('Set-Cookie')).toBeDefined();
   });
 });
