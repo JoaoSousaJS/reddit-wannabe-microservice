5b6570a6c2b2bae1b9ce9fcf0653a2b602cdcfc7
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-27 14:38:26 +0930

    feat: check if user exists on signIn

--- auth/src/controllers/users/sign-in.ts
+++ auth/src/controllers/users/sign-in.ts
@@ -1,5 +1,16 @@
+import { BadRequesterror } from '@reddit-wannabe/common';
 import { Request, Response } from 'express';
+import { User } from '../../database/models/user';
 
-export const signIn = (req: Request, res: Response) => {
+export const signIn = async (req: Request, res: Response) => {
+  const { email } = req.body;
+
+  const existingUser = await User.findOne({
+    email,
+  });
+
+  if (!existingUser) {
+    throw new BadRequesterror('Invalid credentials');
+  }
   res.status(201).send({ status: true });
 };
