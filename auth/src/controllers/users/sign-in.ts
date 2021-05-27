18b0f6801554b7e3c6a15d606ffdf0b697651c43
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-27 14:46:12 +0930

    feat: check if password provided belongs to the user

--- auth/src/controllers/users/sign-in.ts
+++ auth/src/controllers/users/sign-in.ts
@@ -1,15 +1,22 @@
 import { BadRequesterror } from '@reddit-wannabe/common';
 import { Request, Response } from 'express';
 import { User } from '../../database/models/user';
+import { Password } from '../../database/service/password';
 
 export const signIn = async (req: Request, res: Response) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
 
   const existingUser = await User.findOne({
     email,
   });
 
   if (!existingUser) {
+    throw new BadRequesterror('Invalid credentials');
+  }
+
+  const hashedPassword = await Password.compare(existingUser.password, password);
+
+  if (!hashedPassword) {
     throw new BadRequesterror('Invalid credentials');
   }
   res.status(201).send({ status: true });
