b746a67bff0a673042c7a3b8cd0b02d20f11120e
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-27 14:17:25 +0930

    test: return 400 if email is blank

---
+++ auth/src/controllers/users/__test__/sign-in.spec.ts
@@ -0,0 +1,19 @@
+import request from 'supertest';
+import { app } from '../../../app';
+import { User } from '../../../database/models/user';
+import { clear, close, connect } from '../../../test/setup';
+
+const agent = request.agent(app);
+
+describe('Sign in Controller', () => {
+  beforeAll(async () => connect());
+  beforeEach(async () => clear());
+  afterAll(async () => close());
+
+  it('should return 400 if email is blank', async () => {
+    await agent.post('/api/users/signin').send({
+      email: '',
+      password: 'asdfasdfasd',
+    }).expect(400);
+  });
+});
