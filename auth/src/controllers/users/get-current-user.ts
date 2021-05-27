3de2290a4fc6f44d612be0c302c3628afd93ad16
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-27 16:40:58 +0930

    feat: return the details of the logged user

--- auth/src/controllers/users/get-current-user.ts
+++ auth/src/controllers/users/get-current-user.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
 
 export const getCurrentUser = (req: Request, res: Response) => {
-  res.status(200).send({ status: true });
+  res.send({ currentUser: req.currentUser || null });
 };
