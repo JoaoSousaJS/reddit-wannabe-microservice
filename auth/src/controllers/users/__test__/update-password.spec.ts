203b5e9f4687cab864eaef543bcc8577930f054a
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-28 14:25:02 +0930

    feat: add compare password method to test

--- auth/src/controllers/users/__test__/update-password.spec.ts
+++ auth/src/controllers/users/__test__/update-password.spec.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../../app';
 import { User } from '../../../database/models/user';
+import { Password } from '../../../database/service/password';
 import { clear, close, connect } from '../../../test/setup';
 
 const agent = request.agent(app);
@@ -33,6 +34,8 @@ describe('Update Password Controller', () => {
       confirNewPassword: '1234567',
     }).expect(201);
 
-    expect(newUser.password).toEqual('1234567');
+    const hashedPassword = await Password.compare(newUser.password, '1234567');
+
+    expect(hashedPassword).toBeTruthy();
   });
 });
