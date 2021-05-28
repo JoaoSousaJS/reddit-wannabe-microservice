ff5457207b10e60cf2e9cdf367efd0a55bd1536a
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-28 11:35:11 +0930

    fix: fix export

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,6 +1,7 @@
 export * from './errors/bad-request-error'
 export * from './errors/interface/custom-error'
 export * from './errors/request-validation-error'
+export * from './errors/not-found-error'
 
 export * from './middlewares/validate-request'
 export * from './middlewares/error-handler'
