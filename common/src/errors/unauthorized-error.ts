8cfb629a31d6c6dbdf561f039ba66f0dc2d7ba50
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-30 23:29:44 +0930

    feat: add unauthorized error

---
+++ common/src/errors/unauthorized-error.ts
@@ -0,0 +1,16 @@
+import { CustomError } from "./interface/custom-error";
+
+export class UnauthorizedError extends CustomError {
+  statusCode = 401
+
+  constructor(public message: string) {
+    super(message)
+    Object.setPrototypeOf(this, UnauthorizedError.prototype)
+  }
+
+  serializeErrors() {
+    return [{
+      message: this.message
+    }]
+  }
+}
