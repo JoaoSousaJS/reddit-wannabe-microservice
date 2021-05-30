9ea0a968fb440a68dc846e92e0ba3da67a8d5dea
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-30 23:39:25 +0930

    feat: return 400 if user id does not exist

--- auth/src/controllers/users/delete-user.ts
+++ auth/src/controllers/users/delete-user.ts
@@ -1,12 +1,19 @@
-import { UnauthorizedError } from '@reddit-wannabe/common';
+import { BadRequesterror, UnauthorizedError } from '@reddit-wannabe/common';
 import { Request, Response } from 'express';
+import { User } from '../../database/models/user';
 
 export const deleteUser = async (req: Request, res: Response) => {
   const { userId } = req.params;
   const { id } = req.currentUser;
 
+  const user = await User.findById(userId);
+
   if (userId === id) {
     throw new UnauthorizedError("You can't do that");
+  }
+
+  if (!user) {
+    throw new BadRequesterror('User does not exist');
   }
 
   res.send({ true: 'true' });
