ab791e135deaebf21a42fc1c97702a97cf3095ac
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-31 14:22:04 +0930

    feat: return 401 if user is equal to current user

--- auth/src/controllers/users/restore-user.ts
+++ auth/src/controllers/users/restore-user.ts
@@ -1,14 +1,19 @@
-import { BadRequesterror } from '@reddit-wannabe/common';
+import { BadRequesterror, UnauthorizedError } from '@reddit-wannabe/common';
 import { Request, Response } from 'express';
 import { User } from '../../database/models/user';
 
 export const restoreUser = async (req: Request, res: Response) => {
   const { userId } = req.params;
+  const { id } = req.currentUser;
 
   const userExist = await User.findById(userId);
 
   if (!userExist) {
     throw new BadRequesterror('User does not exist');
+  }
+
+  if (userId === id) {
+    throw new UnauthorizedError("You can't do that");
   }
   res.send({ true: 'true' });
 };
