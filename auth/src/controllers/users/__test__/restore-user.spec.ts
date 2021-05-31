18211d6102cadef1a174f0b0c5d05894357749ef
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-31 13:51:06 +0930

    test: should restore an user

---
+++ auth/src/controllers/users/__test__/restore-user.spec.ts
@@ -0,0 +1,38 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+import { app } from '../../../app';
+import { User } from '../../../database/models/user';
+import { clear, close, connect } from '../../../test/setup';
+import { UserStatus } from '../../../database/types/user-status';
+
+const agent = request.agent(app);
+
+const buildUser = async () => {
+  const user = User.build({
+    firstName: 'joao',
+    lastName: 'sousa',
+    email: 'joao@joao.com',
+    password: 'asdfasdfasd',
+  });
+
+  await user.save();
+
+  return user;
+};
+
+describe('Restore User Controller', () => {
+  beforeAll(async () => connect());
+  beforeEach(async () => clear());
+  afterAll(async () => close());
+
+  it('should restore an user', async () => {
+    const newUser = await buildUser();
+
+    await agent.patch(`/api/users/restore/${newUser.id}`).set('Cookie', await global.signIn())
+    .send().expect(200);
+
+    const user = await User.findById(newUser.id);
+
+    expect(user.status).toEqual(UserStatus.Active);
+  });
+});
