5b4097942afc63980d476c2fdbc3d9f5548f4114
Joao Sousa <joao.rsousa23@gmail.com>
2021-05-31 14:55:34 +0930

    fix: fix typo

--- auth/src/controllers/users/__test__/update-user-details.spec.ts
+++ auth/src/controllers/users/__test__/update-user-details.spec.ts
@@ -75,7 +75,7 @@ describe('Update User Detail Controller', () => {
     }).expect(400);
   });
 
-  it('should return 400 if firstName is blank', async () => {
+  it('should return 400 if lastName is blank', async () => {
     const newUser = await buildUser();
 
     await agent.patch(`/api/users/update/${newUser.id}`).set('Cookie', global.signInWithUser(
@@ -103,7 +103,7 @@ describe('Update User Detail Controller', () => {
     expect(response.body.lastName).toEqual('Rivia');
   });
 
-  it('should update the last name of the user', async () => {
+  it('should update the firstName of the user', async () => {
     const newUser = await buildUser();
 
     const response = await agent.patch(`/api/users/update/${newUser.id}`).set('Cookie', global.signInWithUser(
