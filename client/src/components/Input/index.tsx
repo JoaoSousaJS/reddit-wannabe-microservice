94da5d366d689bd7533b350e897efba9f14340db
Joao Sousa <joao.rsousa23@gmail.com>
2021-06-01 16:58:17 +0930

    feat: add onChange method

--- client/src/components/Input/index.tsx
+++ client/src/components/Input/index.tsx
@@ -1,15 +1,25 @@
 import { Text, Input } from '@chakra-ui/react'
+import { useState } from 'react'
 type InputTypes = {
   inputName: string
-  value: string
   placeholder: string
 }
 
-const InputValue = ({ inputName, value, placeholder }: InputTypes) => {
+const InputValue = ({ inputName, placeholder }: InputTypes) => {
+  const [value, setValue] = useState('')
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setValue(event.currentTarget.value)
+
   return (
     <>
       <Text>{inputName}</Text>
-      <Input value={value} placeholder={placeholder} />
+      <Input
+        size="md"
+        onChange={handleChange}
+        value={value}
+        placeholder={placeholder}
+      />
     </>
   )
 }
