f86a53e92525cd906d621591211f280324b196a3
Joao Sousa <joao.rsousa23@gmail.com>
2021-06-03 16:24:22 +0930

    feat: return 400 if title is blank

--- thread/src/routes/index.ts
+++ thread/src/routes/index.ts
@@ -1,7 +1,10 @@
-import { requireAuth } from '@reddit-wannabe/common';
+import { requireAuth, validateRequest } from '@reddit-wannabe/common';
 import express from 'express';
+import { body } from 'express-validator';
 import { newThread } from '../controllers/threads/new-thread';
 
 export const threadRouter = express.Router();
 
-threadRouter.post('/api/threads', requireAuth, newThread);
+threadRouter.post('/api/threads', requireAuth, [
+  body('title').notEmpty().withMessage('Title is required'),
+], validateRequest, newThread);
