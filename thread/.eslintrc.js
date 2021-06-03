ca3cdc1fe5aea02b1efc1b80de5f04491ee99568
Joao Sousa <joao.rsousa23@gmail.com>
2021-06-03 10:49:59 +0930

    feat: add thread service

---
+++ thread/.eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base'],
+  parser: '@typescript-eslint/parser',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  overrides: [
+    {
+      files: [
+        '**/*.spec.ts',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    'no-use-before-define': 'off',
+    indent: 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+};
