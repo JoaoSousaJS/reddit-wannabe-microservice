b10fc7da374519e1e3930264ad39274e37a1d01e
Joao Sousa <joao.rsousa23@gmail.com>
2021-06-04 14:36:28 +0930

    feat: add nextAuth provider

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,13 +2,16 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
 import Header from 'components/Header'
 import theme from '../../styles/theme'
+import { Provider as AuthProvider } from 'next-auth/client'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Header />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <AuthProvider session={pageProps.session}>
+      <ChakraProvider resetCSS theme={theme}>
+        <Header />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AuthProvider>
   )
 }
 
