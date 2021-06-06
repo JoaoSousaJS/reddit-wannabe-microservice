4e93806a716c225a4a60bffbf40c3d422fdbbcf5
Joao Sousa <joao.rsousa23@gmail.com>
2021-06-06 14:54:53 +0930

    feat: add Thread Model

---
+++ thread/src/database/model/thread/index.ts
@@ -0,0 +1,74 @@
+import {
+ Document, model, Model, Schema,
+} from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+import { ThreadStatus } from '../../types/thread-status';
+
+interface ThreadAttrs {
+  id: string
+  title: string
+  userId: string
+  createdAt: Date
+  version: number
+  status: ThreadStatus
+  postId?: [string]
+}
+
+interface ThreadDocument extends Document {
+  title: string
+  userId: string
+  createdAt: Date
+  version: number
+  status: ThreadStatus
+  postId?: [string]
+}
+
+interface ThreadModel extends Model<ThreadDocument> {
+  // eslint-disable-next-line no-unused-vars
+  build(attrs: ThreadAttrs): ThreadDocument
+}
+
+const threadSchema = new Schema<ThreadDocument, ThreadModel>({
+  title: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    tyoe: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: [ThreadStatus],
+    default: ThreadStatus.Active,
+  },
+  postId: {
+    type: Array,
+  },
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      ret.id = ret._id;
+      delete ret._id;
+    },
+  },
+});
+
+threadSchema.set('versionKey', 'version');
+threadSchema.plugin(updateIfCurrentPlugin);
+
+threadSchema.statics.build = (attrs: ThreadAttrs) => new Thread({
+  _id: attrs.id,
+  version: attrs.version,
+  title: attrs.title,
+  status: ThreadStatus.Active,
+  userId: attrs.userId,
+});
+
+export const Thread = model<ThreadDocument, ThreadModel>('Thread', threadSchema);
