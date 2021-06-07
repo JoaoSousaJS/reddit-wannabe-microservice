3e8488fb6c979ec61a4b09e30fdd14b82b39dcd5
Joao Sousa <joao.rsousa23@gmail.com>
2021-06-07 18:20:06 +0930

    test: get all posts attached to the thread

--- thread/src/controllers/__test__/get-all-threads.spec.ts
+++ thread/src/controllers/__test__/get-all-threads.spec.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import mongoose from 'mongoose';
 import { app } from '../../app';
 import { clear, close, connect } from '../../test/setup';
+import { Post } from '../../database/model/post';
 
 const agent = request.agent(app);
 
@@ -31,5 +32,32 @@ describe('Get All Thread', () => {
     const response = await agent.get('/api/threads');
 
     expect(response.body).toHaveLength(2);
+  });
+
+  it('Should list all posts attached to the threads', async () => {
+    const userId = mongoose.Types.ObjectId().toHexString();
+    const postOne = Post.build({
+      title: 'Nintendo',
+      totalComments: 2,
+    });
+
+    await postOne.save();
+
+    const postTwo = Post.build({
+      title: 'Ps5',
+      totalComments: 12,
+    });
+
+    await postTwo.save();
+
+    await agent.post('/api/threads').set('Cookie', global.signIn()).send({
+      title: 'Games 2',
+      userId,
+      post: [postOne.id, postTwo.id],
+    }).expect(201);
+
+    const response = await agent.get('/api/threads');
+
+    expect(response.body[0].post).toHaveLength(2);
   });
 });
