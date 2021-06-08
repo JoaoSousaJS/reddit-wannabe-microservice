31dcb7e819d0875689e255de649491d12dd09864
Joao Sousa <joao.rsousa23@gmail.com>
2021-06-08 13:13:55 +0930

    test: should return the thread details with posts

--- thread/src/controllers/__test__/get-thread.spec.ts
+++ thread/src/controllers/__test__/get-thread.spec.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import { app } from '../../app';
 import { Thread } from '../../database/model/thread';
 import { clear, close, connect } from '../../test/setup';
+import { Post } from '../../database/model/post';
 
 const agent = request.agent(app);
 
@@ -10,6 +11,17 @@ describe('Get specific Thread', () => {
   beforeAll(async () => connect());
   beforeEach(async () => clear());
   afterAll(async () => close());
+
+  it('Should handler listening to /api/threads/:threadId for threads requests', async () => {
+    const response = await agent.get('/api/threads/:threadId').send({});
+
+    expect(response.status).not.toEqual(404);
+  });
+
+  it('Should return 400 if the thread does not exist', async () => {
+    await agent.get('/api/threads/:threadId').expect(400);
+  });
+
   it('Should return the thread details without posts', async () => {
     const randomUserId = mongoose.Types.ObjectId().toHexString();
     const newThread = Thread.build({
@@ -23,13 +35,27 @@ describe('Get specific Thread', () => {
 
     expect(response.body.title).toEqual('games');
   });
-  it('Should handler listening to /api/threads/:threadId for threads requests', async () => {
-    const response = await agent.get('/api/threads/:threadId').send({});
 
-    expect(response.status).not.toEqual(404);
-  });
+  it('Should return the thread details with posts', async () => {
+    const randomUserId = mongoose.Types.ObjectId().toHexString();
+    const newPost = Post.build({
+      title: 'games post',
+      totalComments: 5,
+    });
 
-  it('Should return 400 if the thread does not exist', async () => {
-    await agent.get('/api/threads/:threadId').expect(400);
+    await newPost.save();
+
+    const newThread = Thread.build({
+      title: 'games',
+      userId: randomUserId,
+      post: newPost.id,
+    });
+
+    await newThread.save();
+
+    const response = await agent.get(`/api/threads/${newThread.id}`).expect(200);
+
+    expect(response.body.title).toEqual('games');
+    expect(response.body.post.title).toEqual('games post');
   });
 });
