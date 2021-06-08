5fa2295b6ee08ac7e3ce22a8506a629f96ad9d1d
Joao Sousa <joao.rsousa23@gmail.com>
2021-06-08 15:22:30 +0930

    feat: should return 400 if thread does not exist

--- thread/src/controllers/threads/update-thread.ts
+++ thread/src/controllers/threads/update-thread.ts
@@ -1,5 +1,14 @@
+import { BadRequesterror } from '@reddit-wannabe/common';
 import { Request, Response } from 'express';
+import { Thread } from '../../database/model/thread';
 
 export const updateThread = async (req: Request, res: Response) => {
-  res.status(201).send({ true: 'true' });
+  const { threadId } = req.params;
+
+  const threadExists = await Thread.findById(threadId);
+
+  if (!threadExists) {
+    throw new BadRequesterror('This thread does not exist');
+  }
+  res.status(204).send({ true: 'true' });
 };
