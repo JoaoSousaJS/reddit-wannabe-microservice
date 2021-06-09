0dd4b4accaeefaa986727af3f06722a6f35c83fa
Joao Sousa <joao.rsousa23@gmail.com>
2021-06-09 09:43:16 +0930

    feat: should return 400 if thread does not exist

--- thread/src/controllers/threads/delete-thread.ts
+++ thread/src/controllers/threads/delete-thread.ts
@@ -1,5 +1,14 @@
+import { BadRequesterror } from '@reddit-wannabe/common';
 import { Request, Response } from 'express';
+import { Thread } from '../../database/model/thread';
 
-export const deleteThread = (req:Request, res: Response) => {
+export const deleteThread = async (req: Request, res: Response) => {
+  const { threadId } = req.params;
+
+  const threadExists = await Thread.findById(threadId);
+
+  if (!threadExists) {
+    throw new BadRequesterror('This thread does not exist');
+  }
   res.send({ true: 'true' });
 };
