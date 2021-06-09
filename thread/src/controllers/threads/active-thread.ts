1c58287914a64f487de2e170bdaf58a4e1302e48
Joao Sousa <joao.rsousa23@gmail.com>
2021-06-09 13:38:53 +0930

    feat: return 400 if thread is already deleted

--- thread/src/controllers/threads/active-thread.ts
+++ thread/src/controllers/threads/active-thread.ts
@@ -1,5 +1,14 @@
+import { BadRequesterror } from '@reddit-wannabe/common';
 import { Request, Response } from 'express';
+import { Thread } from '../../database/model/thread';
 
 export const activeThread = async (req: Request, res: Response) => {
+  const { threadId } = req.params;
+
+  const threadExists = await Thread.findById(threadId);
+
+  if (!threadExists) {
+    throw new BadRequesterror('Thread does no exist');
+  }
   res.send({ true: 'true' });
 };
