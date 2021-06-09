5c04cac822f944bf2400ade5e24ca5679da77d1d
Joao Sousa <joao.rsousa23@gmail.com>
2021-06-09 09:53:27 +0930

    feat: return 401 if the thread does not belong to the current user

--- thread/src/controllers/threads/delete-thread.ts
+++ thread/src/controllers/threads/delete-thread.ts
@@ -1,14 +1,19 @@
-import { BadRequesterror } from '@reddit-wannabe/common';
+import { BadRequesterror, UnauthorizedError } from '@reddit-wannabe/common';
 import { Request, Response } from 'express';
 import { Thread } from '../../database/model/thread';
 
 export const deleteThread = async (req: Request, res: Response) => {
   const { threadId } = req.params;
+  const { id } = req.currentUser;
 
   const threadExists = await Thread.findById(threadId);
 
   if (!threadExists) {
     throw new BadRequesterror('This thread does not exist');
+  }
+
+  if (id !== threadExists.userId) {
+    throw new UnauthorizedError('Unauthorized');
   }
   res.send({ true: 'true' });
 };
