07133080098431b1fb6c606667a8cfeb6f3b984b
Joao Sousa <joao.rsousa23@gmail.com>
2021-06-10 11:40:16 +0930

    test: should listen to /api/posts

---
+++ post/src/controllers/__test__/new-post.spec.ts
@@ -0,0 +1,26 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { clear, close, connect } from '../../test/setup';
+
+const agent = request.agent(app);
+
+describe('New Thread', () => {
+  beforeAll(async () => connect());
+  beforeEach(async () => clear());
+  afterAll(async () => close());
+  it('Should handler listening to /api/posts for threads requests', async () => {
+    const response = await agent.post('/api/posts').send({});
+
+    expect(response.status).not.toEqual(404);
+  });
+
+  it('Should only be accessed if user is signed in', async () => {
+    await agent.post('/api/posts').send({}).expect(401);
+  });
+
+  it('Should return a status other than 401 if the user is signed in', async () => {
+    const response = await agent.post('/api/posts').set('Cookie', global.signIn()).send({});
+
+    expect(response.status).not.toEqual(401);
+  });
+});
