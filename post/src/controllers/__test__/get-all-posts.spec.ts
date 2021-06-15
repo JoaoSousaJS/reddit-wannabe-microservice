c8e941ecb5a07ce15ef4d254017fd9e50361b97e
Joao Sousa <joao.rsousa23@gmail.com>
2021-06-15 11:52:22 +0930

    test: should return the post

--- post/src/controllers/__test__/get-all-posts.spec.ts
+++ post/src/controllers/__test__/get-all-posts.spec.ts
@@ -1,5 +1,7 @@
+import { ThreadStatus } from '@reddit-wannabe/common';
 import request from 'supertest';
 import { app } from '../../app';
+import { Thread } from '../../database/model/thread';
 import { clear, close, connect } from '../../test/setup';
 
 const agent = request.agent(app);
@@ -15,6 +17,26 @@ describe('Get All Posts', () => {
   });
 
   it('Should return 400 if the thread does not exist', async () => {
-    await agent.get('/api/threads/:theadId/posts').send({}).expect(400);
+    await agent.get('/api/threads/:theadId/posts').expect(400);
+  });
+
+  it('Should return the post ', async () => {
+    const thread = Thread.build({
+      status: ThreadStatus.Active,
+    });
+
+    await thread.save();
+
+    await agent.post(`/api/threads/${thread.id}/posts`).set('Cookie', global.signIn()).send({
+      title: 'game post',
+    }).expect(201);
+
+    await agent.post(`/api/threads/${thread.id}/posts`).set('Cookie', global.signIn()).send({
+      title: 'game post 2',
+    }).expect(201);
+
+    const response = await agent.get('/api/threads/:theadId/posts').expect(200);
+
+    expect(response.body).toHaveLength(2);
   });
 });
