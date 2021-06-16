73acbdeb825449ba84c7908277d5570c621e1bef
Joao Sousa <joao.rsousa23@gmail.com>
2021-06-16 12:43:01 +0930

    test: should listen to /api/threads/:threadId/posts/:postId

---
+++ post/src/controllers/__test__/get-posts.spec.ts
@@ -0,0 +1,20 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { clear, close, connect } from '../../test/setup';
+
+const agent = request.agent(app);
+
+describe('Get Posts', () => {
+  beforeAll(async () => connect());
+  beforeEach(async () => clear());
+  afterAll(async () => close());
+  it('Should handler listening to /api/posts for posts requests', async () => {
+    const response = await agent.get('/api/threads/:theadId/posts/:postId').send({});
+
+    expect(response.status).not.toEqual(404);
+  });
+
+  it('Should return 400 if the thread does not exist', async () => {
+    await agent.get('/api/threads/:theadId/posts/:postId').expect(400);
+  });
+});
