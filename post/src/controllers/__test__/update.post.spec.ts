ad6a8be71ccefb9dbd295dddef4cff3ca07cb741
Joao Sousa <joao.rsousa23@gmail.com>
2021-06-16 21:13:17 +0930

    test: should listen to /api/threads/:threadId/posts/:postId

---
+++ post/src/controllers/__test__/update.post.spec.ts
@@ -0,0 +1,26 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { clear, close, connect } from '../../test/setup';
+
+const agent = request.agent(app);
+
+describe('Update Post', () => {
+  beforeAll(async () => connect());
+  beforeEach(async () => clear());
+  afterAll(async () => close());
+  it('Should handler listening to /api/threads/:theadId/posts/:postId for posts requests', async () => {
+    const response = await agent.put('/api/threads/:theadId/posts/:postId').send({});
+
+    expect(response.status).not.toEqual(404);
+  });
+
+  it('Should only be accessed if user is signed in', async () => {
+    await agent.put('/api/threads/:theadId/posts/:postId').send({}).expect(401);
+  });
+
+  it('Should return a status other than 401 if the user is signed in', async () => {
+    const response = await agent.put('/api/threads/:theadId/posts/:postId').set('Cookie', global.signIn()).send({});
+
+    expect(response.status).not.toEqual(401);
+  });
+});
